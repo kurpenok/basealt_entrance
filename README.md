dfbe7a4fb18ef73e394de31f4966e990bd37df1b
Kurpenov Kuat <kurpenov2003@yandex.ru>
2024-05-06 04:05:40 +0600

    Updated readme and added import .so in cli

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Basealt entrance task
+# ALT Package Difference
+
+### How to build .so
+
+```
+cd apd_core
+cargo build --release
+```
+
+### How to run CLI
+```
+cd ..
+cd apd
+cp ../apd_core/target/release/libapd_core.so target/debug/
+
+cargo run -- --repo-1 p9 --repo-2 p10
+```
--- apd/src/main.rs
+++ apd/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        let lib = libloading::Library::new("libapd_core.so").unwrap();
+
+        let get_branch_packages: libloading::Symbol<unsafe extern "C" fn(&str)> =
+            lib.get(b"get_branch_packages\0").unwrap();
+
+        get_branch_packages("p9");
+    }
 }
